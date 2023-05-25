a8144f1ddd1a0c9c0e9500e28392ede64d84fc31
Nathan Surgenor <ndsurgenor@gmail.com>
2023-05-25 22:39:20 +0000

    Add while loop to continue until valid data is entered

--- run.py
+++ run.py
@@ -16,18 +16,23 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-
 def get_sales_data():
     """
     Get sales figures from the user
     """
-    print('Please enter sales data from the last market.')
-    print('Data should be six numbers, separated by commas.')
-    print('Example: 10,20,30,40,50,60\n')
+    while True:
+        print('Please enter sales data from the last market.')
+        print('Data should be six numbers, separated by commas.')
+        print('Example: 10,20,30,40,50,60\n')
 
-    data_str = input("Enter your data here: ")
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        data_str = input("Enter your data here: ")
+        sales_data = data_str.split(",")
+        
+        if validate_data(sales_data):
+            print('Valid data entered.')
+            break
+    
+    return sales_data
 
 
 def validate_data(values):
@@ -44,6 +49,9 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
+    
+    return True
 
 
-get_sales_data()
+data = get_sales_data()
